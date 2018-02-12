290b47efcdefc10a98b9269d4db605952e8c3d6e
Bart Massey <bart@cs.pdx.edu>
2018-02-11 22:40:12 -0800

    "upgraded" from functions to class

--- minmaxheap.py
+++ minmaxheap.py
@@ -1,197 +1,201 @@
 # Copyright (c) 2018 Bart Massey
 
-# Min-Max Heap
-#
-#   Min-Max Heaps and Generalized Priority Queues
-#   M.D. Atkinson, J.-R. Sack, N. Santoro, T. Strothotte
-#   Comms ACM 29(10) p. 996, October 1986
-#   http://cglab.ca/~morin/teaching/5408/refs/minmax.pdf
-#
-# See also
-#   Diamond deque: a simple data structure for priority deques
-#   S.C. Chang, M.W. Du
-#   Info Proc Letters 46(5) p. 231, July 1993
-# for what would probably be a cleaner and easier data structure.
+"""
+Min-Max Heap
+
+A min-max heap (double-ended priority heap) allows
+efficient extraction of either min or max elements.
+
+Implementation of pseudocode from
+
+  Min-Max Heaps and Generalized Priority Queues
+  M.D. Atkinson, J.-R. Sack, N. Santoro, T. Strothotte
+  Comms ACM 29(10) p. 996, October 1986
+  http://cglab.ca/~morin/teaching/5408/refs/minmax.pdf
+
+See also
+
+  Diamond deque: a simple data structure for priority deques
+  S.C. Chang, M.W. Du
+  Info Proc Letters 46(5) p. 231, July 1993
+
+for what would probably be a cleaner and easier data structure.
+"""
 
 import random
 
-def is_min_level(index):
-    "Tree level of index is some min level."
-    index += 1
-    return (index.bit_length() & 1) == 1
+class MinMaxHeap(object):
+    def __init__(self, a, start=0, end=None, lt=None):
+        """Create a new heap with the given start and end
+        elements (end is actually size, i.e. last element
+        plus one) and comparison function."""
+        self.a = a
+        self.start = start
+        if end == None:
+            end = len(a) - start
+        self.end = end
+        assert self.end >= self.start
+        if lt == None:
+            lt = lambda x, y: x < y
+        self.lt = lt
+        self.gt = lambda x, y: lt(y, x)
+        if self.end > self.start + 1:
+            self.heapify()
 
-def downheap(a, start=0, end=None, lt=None):
-    "Downheap minmax heap a starting at position i."
-    if end == None:
-        end = len(a)
-    if lt == None:
-        lt = lambda x, y: x < y
-    if is_min_level(start):
-        cf = lt
-    else:
-        cf = lambda x, y: lt(y, x)
-    assert end - start > 0
-    left = 2 * start + 1
-    while left < end:
-        right = left + 1
-        nexti = left
-        grandchildren = list(range(2 * left + 1, 2 * left + 5))
-        for i in [right] + grandchildren:
-            if i >= end:
-                break
-            if cf(a[i], a[nexti]):
-                nexti = i
-        if nexti <= right:
-            if cf(a[nexti], a[start]):
-                a[nexti], a[start] = a[start], a[nexti]
-            return
+    def swap(self, i, j):
+        "Exchange a[i] with a[j]."
+        self.a[i], self.a[j] = self.a[j], self.a[i] 
+
+    def is_min_level(self, index):
+        "Tree level of index is some min level."
+        index = index - self.start + 1
+        return (index.bit_length() & 1) == 1
+
+    def downheap(self, i=None):
+        "Downheap minmax heap a starting at position i."
+        if i == None:
+            i = self.start
+        assert i >= self.start and i < self.end
+        i0 = i
+        if self.is_min_level(i):
+            cf = self.lt
         else:
-            if cf(a[nexti], a[start]):
-                a[nexti], a[start] = a[start], a[nexti]
-                parent = (nexti - 1) // 2
-                if cf(a[parent], a[nexti]):
-                    a[nexti], a[parent] = a[parent], a[nexti]
-            else:
+            cf = self.gt
+        left = 2 * i + 1
+        while left < self.end:
+            right = left + 1
+            nexti = left
+            for c in [right, 2*left+1, 2*left+2, 2*right+1, 2*right+2]:
+                if c >= self.end:
+                    break
+                if cf(self.a[c], self.a[nexti]):
+                    nexti = c
+            if nexti <= right:
+                if cf(self.a[nexti], self.a[i]):
+                    self.swap(nexti, i)
                 return
-        start = nexti
-        left = 2 * start + 1
+            else:
+                if cf(self.a[nexti], self.a[i]):
+                    self.swap(nexti, i)
+                    parent = (nexti - 1) // 2
+                    if cf(self.a[parent], self.a[nexti]):
+                        self.swap(nexti, parent)
+                else:
+                    return
+            i = nexti
+            left = 2 * i + 1
 
-def upheap(a, start=None, end=None, lt=None):
-    "Upheap minmax heap a starting at position i."
-    if end == None:
-        end = len(a)
-    if start == None:
-        start = end - 1
-    if lt == None:
-        lt = lambda x, y: x < y
-    assert end - start > 0
-    if start == 0:
-        return
-    gt = lambda x, y: lt(y, x)
-    parent = (start - 1) // 2
-    if is_min_level(start):
-        if gt(a[start], a[parent]):
-            a[start], a[parent] = a[parent], a[start]
-            start = parent
-            cf = gt
+    def upheap(self, a, i=None):
+        """Upheap minmax heap a starting at position i."""
+        if i == None:
+            i = self.end - 1
+        assert i >= self.start and i < self.end
+        if i == self.start:
+            return
+        parent = (i - 1) // 2
+        if self.is_min_level(i):
+            if self.gt(self.a[i], self.a[parent]):
+                self.swap(i, parent)
+                i = parent
+                cf = self.gt
+            else:
+                cf = self.lt
         else:
-            cf = lt
-    else:
-        if lt(a[start], a[parent]):
-            a[start], a[parent] = a[parent], a[start]
-            start = parent
-            cf = lt
-        else:
-            cf = gt
-    parent = (start - 1) // 2
-    grandparent = (parent - 1) // 2
-    while start >= 3 and cf(a[start], a[grandparent]):
-        a[start], a[grandparent] = a[grandparent], a[start]
-        start = grandparent
-        parent = (start - 1) // 2
+            if self.lt(self.a[i], self.a[parent]):
+                self.swap(i, parent)
+                i = parent
+                cf = self.lt
+            else:
+                cf = self.gt
+        parent = (i - 1) // 2
         grandparent = (parent - 1) // 2
+        while i >= 3 and cf(self.a[i], self.a[grandparent]):
+            self.swap(i, grandparent)
+            i = grandparent
+            parent = (i - 1) // 2
+            grandparent = (parent - 1) // 2
         
-def check_heap(a, start=0, end=None, lt=None):
-    "Highly inefficient check of heap invariant."
-    if end == None:
-        end = len(a)
-    if lt == None:
-        lt = lambda x, y: x < y
+    def increment_end(self):
+        "Bump the end pointer toward end-of-heap."
+        assert self.end < len(self.a)
+        self.end += 1
 
-    def children(i):
-        if i >= end:
-            return []
-        return children(2 * i + 1) + [(i, a[i])] + children(2 * i + 2)
+    def decrement_end(self):
+        "Bump the end pointer toward end-of-heap."
+        assert self.end > self.start
+        self.end -= 1
 
-    def check_posn(p):
-        if is_min_level(p):
-            cf = lambda x, y: x == y or lt(x, y)
-            cfs = "<="
-        else:
-            cf = lambda x, y: x == y or lt(y, x)
-            cfs = ">="
-        for i, ai in children(p):
-            if not cf(a[p], ai):
-                print("failed a[{}]={} {} a[{}]={}".format(
-                    p, a[p], cfs, i, ai))
+    def check_heap(self):
+        "Highly inefficient check of heap invariant."
 
-    for p in range(start, end):
-        check_posn(p)
+        def children(i):
+            if i >= self.end:
+                return []
+            return children(2 * i + 1) + [(i, self.a[i])] + children(2 * i + 2)
 
+        def check_posn(p):
+            if self.is_min_level(p):
+                cf = lambda x, y: x == y or self.lt(x, y)
+                cfs = "<="
+            else:
+                cf = lambda x, y: x == y or self.gt(x, y)
+                cfs = ">="
+            result = True
+            for i, ai in children(p):
+                if not cf(self.a[p], ai):
+                    print("failed a[{}]={} {} a[{}]={}".format(
+                        p, self.a[p], cfs, i, ai))
+                    result = False
+            return result
 
-def heapify(a, lt=None):
-    "Make a into a heap."
-    end = len(a)
-    for i in reversed(range((end-2) // 2 + 1)):
-        downheap(a, start=i, lt=lt)
-        # check_heap(a, start=i)
+        ok = True
+        for p in range(self.start, self.end):
+            ok = ok and check_posn(p)
+        assert ok
 
-def store_min(a, end=None, lt=None):
-    "Extract min element and place it after the end of the heap."
-    if end == None:
-        end = len(a)
-    if lt == None:
-        lt = lambda x, y: x < y
-    assert end > 0
-    if end == 1:
-        return
-    a[0], a[end-1] = a[end-1], a[0]
-    downheap(a, end=end-1, lt=lt)
+    def heapify(self):
+        "Enforce the heap property on our a."
+        for i in reversed(range(self.start, (self.end - 1) // 2 + 1)):
+            self.downheap(i)
 
-def store_max(a, end=None, lt=None):
-    "Extract max element and place it after the end of the heap."
-    if end == None:
-        end = len(a)
-    if lt == None:
-        lt = lambda x, y: x < y
-    assert end > 0
-    if end == 1:
-        return
-    imax = 1
-    if end > 2 and lt(a[1], a[2]):
-        imax = 2
-    a[imax], a[end-1] = a[end-1], a[imax]
-    downheap(a, start=imax, end=end-1, lt=lt)
+    def store_min(self):
+        "Extract min element and place it after the new end of the heap."
+        assert self.end >= self.start
+        self.end -= 1
+        if self.end <= self.start:
+            return
+        self.swap(self.start, self.end)
+        self.downheap()
 
-def peek_min(a, end=None, lt=None):
-    "Return min element."
-    if end == None:
-        end = len(a)
-    if lt == None:
-        lt = lambda x, y: x < y
-    assert end > 0
-    return a[0]
-
-def peek_max(a, end=None, lt=None):
-    "Return max element."
-    if end == None:
-        end = len(a)
-    if lt == None:
-        lt = lambda x, y: x < y
-    assert end > 0
-    if end == 1:
-        return a[0]
-    imax = 1
-    if end > 2 and lt(a[1], a[2]):
-        imax = 2
-    return a[imax]
+    def store_max(self):
+        "Extract max element and place it after the new end of the heap."
+        assert self.end >= self.start
+        self.end -= 1
+        if self.end <= self.start:
+            return
+        imax = self.start + 1
+        if self.end > imax and self.lt(self.a[imax], self.a[imax + 1]):
+            imax += 1
+        self.swap(imax, self.end)
+        self.downheap(imax)
 
 if __name__ == "__main__":
 
-    a = [chr(ord('a') + i) for i in range(15)]
-    random.shuffle(a)
-    heapify(a)
-    check_heap(a)
-    n = len(a)
+    s = [chr(ord('a') + i) for i in range(15)]
+    random.shuffle(s)
+    h = MinMaxHeap(s)
+    h.check_heap()
+    n = len(s)
     small_half =  n // 2
-    for end in reversed(range(small_half+1, n+1)):
-        store_max(a, end=end)
-    for end in reversed(range(1, small_half+1)):
-        store_min(a, end=end)
-    assert ''.join(a) == "gfedcbahijklmno"
+    for _ in range(small_half, n):
+        h.store_max()
+    for _ in range(0, small_half):
+        h.store_min()
+    assert ''.join(s) == "gfedcbahijklmno"
 
-    random.shuffle(a)
+    random.shuffle(s)
+    h = MinMaxHeap(s, end=0)
     for i in range(n):
-        upheap(a, start=i, end=i+1)
-    check_heap(a)
-    print(''.join(a))
+        h.increment_end()
+        h.upheap(i)
+    h.check_heap()
